01aa537734dbacdea10a878df787c10ca9e3d82c
PawanAK <pawanajjark@gmail.com>
2024-02-04 10:05:28 +0530

    Add routes for users, groups, and expenses

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const requireAuth = require('./middleware/requireAuth')
 
 const PORT = 4000
 
+const usersRouter = require('./routes/userRouter')
+const expenseRouter = require('./routes/expenseRouter')
+const groupRouter = require('./routes/groupRouter')
+
 // express app
 const app = express()
+
 
 // middleware
 app.use(express.json())
@@ -17,6 +23,10 @@ app.use((req, res, next) => {
 })
 
 // routes
+
+app.use('/api/users', usersRouter)
+app.use('/api/group', requireAuth, groupRouter)
+app.use('/api/expense', requireAuth, expenseRouter)
 
 
 // connect to db
